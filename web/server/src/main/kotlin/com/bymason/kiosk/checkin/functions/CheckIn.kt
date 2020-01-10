8be7f3b434f3acaf0aed350747b22d2f98a13503
Alex Saveau <alexsaveau@bymason.com>
2020-01-09 18:05:13 -0800

    Add support for SMS notifications
    Signed-off-by: Alex Saveau <alexsaveau@bymason.com>

--- web/server/src/main/kotlin/com/bymason/kiosk/checkin/functions/CheckIn.kt
+++ web/server/src/main/kotlin/com/bymason/kiosk/checkin/functions/CheckIn.kt
@@ -1,5 +1,6 @@
 package com.bymason.kiosk.checkin.functions
 
+import admin_directory_v1.`Schema$User`
 import com.bymason.kiosk.checkin.utils.fetchPopulatedSession
 import com.bymason.kiosk.checkin.utils.getAndInitCreds
 import com.bymason.kiosk.checkin.utils.installGoogleAuth
@@ -8,6 +9,7 @@ import firebase.firestore.FieldValues
 import firebase.firestore.SetOptions
 import firebase.functions.AuthContext
 import firebase.functions.admin
+import firebase.functions.functions
 import firebase.https.CallableContext
 import firebase.https.HttpsError
 import google.google
@@ -121,7 +123,7 @@ private suspend fun finalizeSession(
         hostId: String,
         guestName: String
 ): String {
-    val creds = getAndInitCreds(auth.uid, "gsuite", "slack")
+    val creds = getAndInitCreds(auth.uid, "gsuite")
     val state = installGoogleAuth(creds.getValue("gsuite"))
     val directory = google.admin(json("version" to "directory_v1"))
     val host = directory.users.get(json(
@@ -131,32 +133,7 @@ private suspend fun finalizeSession(
     maybeRefreshGsuiteCreds(auth.uid, state)
     console.log("Host: ", host)
 
-    val hostEmail = host.primaryEmail
-    val slackUser = superagent.get("https://slack.com/api/users.lookupByEmail")
-            .query(json(
-                    "token" to creds.getValue("slack")["access_token"],
-                    "email" to hostEmail
-            ))
-            .await().body
-    console.log("Slack user: ", slackUser)
-
-    if (!slackUser["ok"].unsafeCast<Boolean>()) {
-        throw HttpsError("not-found", slackUser["error"].unsafeCast<String>())
-    }
-
-    val slackUserId = slackUser.asDynamic().user.id
-    val slackMessage = superagent.post("https://slack.com/api/chat.postMessage")
-            .query(json(
-                    "token" to creds.getValue("slack")["access_token"],
-                    "channel" to slackUserId,
-                    "text" to "Your guest ($guestName) just arrived! \uD83D\uDC4B"
-            ))
-            .await().body
-    console.log("Slack message: ", slackUser)
-
-    if (!slackMessage["ok"].unsafeCast<Boolean>()) {
-        throw HttpsError("unknown", slackUser["error"].unsafeCast<String>())
-    }
+    notifyHost(auth, host, guestName)
 
     val sessionDoc = admin.firestore().collection(auth.uid)
             .doc("sessions")
@@ -169,4 +146,95 @@ private suspend fun finalizeSession(
     ), SetOptions.merge).await()
 
     return sessionDoc.id
+}
+
+private suspend fun notifyHost(auth: AuthContext, host: `Schema$User`, guestName: String) {
+    val notificationsSnap = admin.firestore()
+            .collection(auth.uid)
+            .doc("config")
+            .collection("metadata")
+            .doc("notifications")
+            .get()
+            .await()
+
+    val notifyOrder = notificationsSnap.data()["guestArrival"].unsafeCast<Array<Int>>()
+    for (notifyType in notifyOrder) {
+        try {
+            when (notifyType) {
+                1 -> notifyHostViaEmail()
+                2 -> notifyHostViaSlack(auth, host, guestName)
+                3 -> notifyHostViaSms(host, guestName)
+            }
+            break
+        } catch (t: Throwable) {
+            console.log(t)
+
+            if (t.asDynamic().code == "failed-precondition") {
+                continue
+            } else {
+                throw t
+            }
+        }
+    }
+}
+
+private fun notifyHostViaEmail() {
+    error("Email notifications are not yet available")
+}
+
+private suspend fun notifyHostViaSlack(auth: AuthContext, host: `Schema$User`, guestName: String) {
+    val creds = getAndInitCreds(auth.uid, "slack")
+    val hostEmail = host.primaryEmail
+    val slackUser = superagent.get("https://slack.com/api/users.lookupByEmail")
+            .query(json(
+                    "token" to creds.getValue("slack")["access_token"],
+                    "email" to hostEmail
+            ))
+            .await().body
+    console.log("Slack user: ", slackUser)
+
+    if (!slackUser["ok"].unsafeCast<Boolean>()) {
+        throw HttpsError("failed-precondition", slackUser["error"].unsafeCast<String>())
+    }
+
+    val slackUserId = slackUser.asDynamic().user.id
+    val slackMessage = superagent.post("https://slack.com/api/chat.postMessage")
+            .query(json(
+                    "token" to creds.getValue("slack")["access_token"],
+                    "channel" to slackUserId,
+                    "text" to "Your guest ($guestName) just arrived! \uD83D\uDC4B"
+            ))
+            .await().body
+    console.log("Slack message: ", slackMessage)
+
+    if (!slackMessage["ok"].unsafeCast<Boolean>()) {
+        throw HttpsError("unknown", slackMessage["error"].unsafeCast<String>())
+    }
+}
+
+private suspend fun notifyHostViaSms(host: `Schema$User`, guestName: String) {
+    val hostPhone = host.phones.unsafeCast<Array<Json>?>()
+            ?.sortedByDescending { if (it["primary"] == true) 1 else 0 }
+            ?.map { it["value"] as String }
+            ?.firstOrNull()
+    if (hostPhone.isNullOrEmpty()) {
+        throw HttpsError("failed-precondition", "Host has no phone numbers.")
+    }
+
+    val accountSid = functions.config().twilio.sid
+    val userHash = functions.config().twilio.user_hash
+    val sms = superagent.post("https://api.twilio.com/2010-04-01/Accounts/$accountSid/Messages.json")
+            .set(json("Authorization" to "Basic $userHash"))
+            .type("form")
+            .send(json(
+                    "To" to if ("+" in hostPhone) hostPhone else "+1$hostPhone",
+                    "From" to "+12064830420",
+                    "Body" to "Your guest ($guestName) just arrived! \uD83D\uDC4B"
+            ))
+            .await().body
+    console.log("Twilio message: ", sms)
+
+    if (sms["error_code"] != null) {
+        throw HttpsError("unknown", sms["error_message"].unsafeCast<String>())
+    }
 }
