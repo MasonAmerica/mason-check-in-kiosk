0044056eef7d003759c480fb61f3ec5544133093
Alex Saveau <alexsaveau@bymason.com>
2020-03-17 15:41:01 -0700

    Hide keyboard during screenshot tests
    Signed-off-by: Alex Saveau <alexsaveau@bymason.com>

--- app/src/androidTest/java/support/bymason/kiosk/checkin/helpers/Screenshots.kt
+++ app/src/androidTest/java/support/bymason/kiosk/checkin/helpers/Screenshots.kt
@@ -2,10 +2,13 @@ package support.bymason.kiosk.checkin.helpers
 
 import android.app.Activity
 import android.view.ViewGroup
+import android.view.ViewGroup.FOCUS_BLOCK_DESCENDANTS
 import android.widget.EditText
 import androidx.core.view.children
 import androidx.test.core.app.ActivityScenario
 import com.facebook.testing.screenshot.Screenshot
+import support.bymason.kiosk.checkin.core.ui.hideKeyboard
+import java.util.concurrent.Semaphore
 
 fun <T : Activity> ActivityScenario<T>.takeScreenshot() {
     lateinit var activity: Activity
@@ -13,7 +16,7 @@ fun <T : Activity> ActivityScenario<T>.takeScreenshot() {
         activity = it
     }
 
-    activity.hideCursors()
+    activity.stabilizeTests()
     Thread.sleep(750) // Wait for animations to finish
 
     // The automated screenshot naming is based off of stack trace analysis. We can't put this in
@@ -21,13 +24,31 @@ fun <T : Activity> ActivityScenario<T>.takeScreenshot() {
     Screenshot.snapActivity(activity).record()
 }
 
-private fun Activity.hideCursors() {
-    runOnUiThread { findViewById<ViewGroup>(android.R.id.content).hideCursors() }
+private fun Activity.stabilizeTests() {
+    val semaphore = Semaphore(0)
+    runOnUiThread {
+        findViewById<ViewGroup>(android.R.id.content).stabilizeTests()
+
+        semaphore.release()
+    }
+    semaphore.acquire()
+}
+
+private fun ViewGroup.stabilizeTests() {
+    hideCursors()
+    hideKeyboard()
 }
 
 private fun ViewGroup.hideCursors() {
     for (child in children) {
-        if (child is ViewGroup) child.hideCursors()
-        if (child is EditText) child.isCursorVisible = false
+        if (child is ViewGroup) {
+            child.descendantFocusability = FOCUS_BLOCK_DESCENDANTS
+            child.hideCursors()
+        }
+
+        if (child is EditText) {
+            child.clearFocus()
+            child.isCursorVisible = false
+        }
     }
 }
