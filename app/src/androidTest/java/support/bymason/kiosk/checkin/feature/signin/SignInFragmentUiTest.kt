585a328ee2fb0e4e6075aea3a85c163542c56377
Alex Saveau <alexsaveau@bymason.com>
2020-03-17 18:10:39 -0700

    Add filled out sign in page screenshot test
    Signed-off-by: Alex Saveau <alexsaveau@bymason.com>

--- app/src/androidTest/java/support/bymason/kiosk/checkin/feature/signin/SignInFragmentUiTest.kt
+++ app/src/androidTest/java/support/bymason/kiosk/checkin/feature/signin/SignInFragmentUiTest.kt
@@ -1,10 +1,14 @@
 package support.bymason.kiosk.checkin.feature.signin
 
 import androidx.test.core.app.launchActivity
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.typeText
+import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import org.junit.Test
 import org.junit.runner.RunWith
 import support.bymason.kiosk.checkin.HomeActivity
+import support.bymason.kiosk.checkin.R
 import support.bymason.kiosk.checkin.helpers.UiTestBase
 import support.bymason.kiosk.checkin.helpers.takeScreenshot
 
@@ -13,6 +17,16 @@ class SignInFragmentUiTest : UiTestBase() {
     @Test
     fun diffEmptySignInScreen() {
         val scenario = launchActivity<HomeActivity>()
+
+        scenario.takeScreenshot()
+    }
+
+    @Test
+    fun diffFilledOutSignInScreen() {
+        val scenario = launchActivity<HomeActivity>()
+
+        onView(withId(R.id.email)).perform(typeText("hot@sauce.com"))
+        onView(withId(R.id.password)).perform(typeText("password"))
 
         scenario.takeScreenshot()
     }
